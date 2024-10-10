11ccce40b7b07daac4c2e9b80750a8aaebff0271
martleru <68188509+martleru@users.noreply.github.com>
2024-10-10 20:05:55 +0300

    add: Empty state ( when no tasks left) component

---
+++ src/app/components/task-list-empty/task-list-empty.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-list-empty',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-list-empty.component.html',
+  styleUrl: './task-list-empty.component.scss'
+})
+export class TaskListEmptyComponent {
+
+}
