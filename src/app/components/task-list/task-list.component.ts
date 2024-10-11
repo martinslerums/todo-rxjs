f65cf840ed9fc2986c86526dc39939005a23a525
martleru <68188509+martleru@users.noreply.github.com>
2024-10-11 21:54:19 +0300

    add: Styles and Card component

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TaskCardComponent } from "../task-card/task-card.component";
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [],
+  imports: [TaskCardComponent],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss'
 })
