b07d57e6f9682c02f13928f9e69217f8ce1951a3
martleru <68188509+martleru@users.noreply.github.com>
2024-10-15 17:23:26 +0300

    Change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { TodoEntryFormComponent } from "./components/todo-entry-form/todo-entry-form.component";
+import { TaskGridComponent } from "./components/task-grid/task-grid.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, TodoEntryFormComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, TodoEntryFormComponent, TaskGridComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
