056a8c92623aad3f3eac9ec6167637c22004de8d
martleru <68188509+martleru@users.noreply.github.com>
2024-10-20 13:09:27 +0300

    feat: Linked form with tasks data

--- src/app/components/todo-entry-form/todo-entry-form.component.ts
+++ src/app/components/todo-entry-form/todo-entry-form.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-entry-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './todo-entry-form.component.html',
-  styleUrl: './todo-entry-form.component.scss'
+  styleUrl: './todo-entry-form.component.scss',
 })
 export class TodoEntryFormComponent {
+  private readonly taskService = inject(TaskService);
+  title: string = '';
 
+  todos$ = this.taskService.todo$;
+
+  addTask() {
+    if (!this.title) return;
+
+    this.taskService.addTask({
+      title: this.title,
+      completed: false,
+    });
+
+    this.title = '';
+  }
 }
