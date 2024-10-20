8d17be022bbe448e3aebb81cbfda26030917cef7
martleru <68188509+martleru@users.noreply.github.com>
2024-10-20 13:11:00 +0300

    feat: Dynamically drawing tasks data

--- src/app/components/task-grid/task-grid.component.ts
+++ src/app/components/task-grid/task-grid.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
-import { TaskInfoComponent } from "../task-info/task-info.component";
-import { TaskListComponent } from "../task-list/task-list.component";
-import { TaskListEmptyComponent } from "../task-list-empty/task-list-empty.component";
+import { Component, inject } from '@angular/core';
+import { TaskInfoComponent } from '../task-info/task-info.component';
+import { TaskListEmptyComponent } from '../task-list-empty/task-list-empty.component';
+import { TaskService } from '../../services/task.service';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { TaskCardComponent } from '../task-card/task-card.component';
 
 @Component({
   selector: 'app-task-grid',
   standalone: true,
-  imports: [TaskInfoComponent, TaskListComponent, TaskListEmptyComponent],
+  imports: [
+    CommonModule,
+    AsyncPipe,
+    TaskInfoComponent,
+    TaskListEmptyComponent,
+    TaskCardComponent,
+  ],
   templateUrl: './task-grid.component.html',
-  styleUrl: './task-grid.component.scss'
+  styleUrl: './task-grid.component.scss',
+  providers: [TaskService],
 })
 export class TaskGridComponent {
+  total = 0;
+  completed = 0;
 
+  taskService = inject(TaskService);
+
+  $todos = this.taskService.todo$;
+
+  $completed = this.$todos.subscribe((v) => (this.total = v.length));
+  $total = this.taskService.completedTasks$.subscribe(
+    (v) => (this.completed = v.length)
+  );
 }
