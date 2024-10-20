904d3230b7f1fdd5e23ccda2134ca45899d3de99
martleru <68188509+martleru@users.noreply.github.com>
2024-10-20 13:10:35 +0300

    feat: Dynamically marking task completed adds styles.

--- src/app/components/task-card/task-card.component.ts
+++ src/app/components/task-card/task-card.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { ITask } from '../../interfaces/ITask';
+import { CommonModule } from '@angular/common';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-task-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './task-card.component.html',
-  styleUrl: './task-card.component.scss'
+  styleUrl: './task-card.component.scss',
 })
 export class TaskCardComponent {
+  private readonly taskService: TaskService = inject(TaskService);
 
+  @Input({ required: true }) task: ITask | undefined;
+
+  completeTask(id?: number) {
+    if (!id) return;
+    this.taskService.completeTask(id);
+  }
+
+  deleteTask(id?: number) {
+    if (!id) return;
+    this.taskService.deleteTask(id)
+  }
 }
