84c731958f492ea2e391588d0f7ddc7c0dff7b9b
martleru <68188509+martleru@users.noreply.github.com>
2024-10-20 13:09:49 +0300

    add: Dynamically showing data

--- src/app/components/task-info/task-info.component.ts
+++ src/app/components/task-info/task-info.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-task-info',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './task-info.component.html',
-  styleUrl: './task-info.component.scss'
+  styleUrl: './task-info.component.scss',
 })
 export class TaskInfoComponent {
-
+  @Input({ required: true }) isBoth: boolean = false;
+  @Input({ required: true }) total: number = 0;
+  @Input() completed: number = 0;
+  @Input({ required: true }) label: string = '';
 }
